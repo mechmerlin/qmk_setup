66cdd12d82c947cb5a5488f723f7d9d443ebd1f7
MechMerlin <mechmerlin@Administrators-MacBook-Pro.local>
2018-01-01 09:57:15 -0800

    Add avrdude to install list and improve comments for instructions

--- qmksetup_macos.sh
+++ qmksetup_macos.sh
@@ -11,11 +11,20 @@
 # Intended User: A person new to the community or QMK with little to no programming
 #                experience seeking to just get this setup. 
 #
-# To execute: save and type `chmod +x ./qmksetup_macos.sh` then `./qmksetup_macos.sh`
+# Make sure you have administrative privileges, as you will be installing software
+# on this machine.
+#
+# To execute: 
+#     1. Save file to Documents directory
+#     2. Open up terminal app in Applications/Utilities
+#     3. Navigate to Documents by typing in "cd Documents" followed by enter
+#     4. Type `chmod +x ./qmksetup_macos.sh`, then enter
+#     5. Type `./qmksetup_macos.sh`, then enter
 #
 # Example:
 #       you@someapplecomputer: chmod +x ./qmksetup_macos.sh
 #       you@someapplecomputer: ./qmksetup_macos.sh
+#
 
 echo "Installing Homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -38,6 +47,7 @@ brew tap PX4/homebrew-px4
 brew update
 brew install avr-gcc
 brew install dfu-programmer
+brew install avrdude
 brew install gcc-arm-none-eabi
 
 #Clone QMK Firmware from Master - It is recommended to fork the repo and then clone from your fork
